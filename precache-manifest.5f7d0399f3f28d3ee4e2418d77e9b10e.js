faedd5d73e378ac9cb2fe3225cd8c1a9500b2f31
roxanavasile <37909653+roxanavasile@users.noreply.github.com>
2019-02-11 14:40:40 -0500

    Updates

---
+++ precache-manifest.5f7d0399f3f28d3ee4e2418d77e9b10e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "24ae2397df2b53cf6831cc445e117b49",
+    "url": "/InteractiveResume.git/static/media/tiles_wireframe.24ae2397.PNG"
+  },
+  {
+    "revision": "14206dc4d0fa92e66386",
+    "url": "/InteractiveResume.git/static/css/main.494ceb13.chunk.css"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/InteractiveResume.git/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a83900eddfc23dbbfaf8",
+    "url": "/InteractiveResume.git/static/js/1.a83900ed.chunk.js"
+  },
+  {
+    "revision": "014a45d7bd1152f1f97e",
+    "url": "/InteractiveResume.git/static/js/runtime~main.014a45d7.js"
+  },
+  {
+    "revision": "82074d78ce8501286e5e320ef25e811b",
+    "url": "/InteractiveResume.git/static/media/headshot.82074d78.jpg"
+  },
+  {
+    "revision": "dd9d3e9648fd101141bf1a884828154f",
+    "url": "/InteractiveResume.git/static/media/stokes_county.dd9d3e96.PNG"
+  },
+  {
+    "revision": "14206dc4d0fa92e66386",
+    "url": "/InteractiveResume.git/static/js/main.14206dc4.chunk.js"
+  },
+  {
+    "revision": "dcc1a59da2e756683a9c578f8794f4c4",
+    "url": "/InteractiveResume.git/static/media/Blitmore.dcc1a59d.png"
+  },
+  {
+    "revision": "2f88349b4d743b7be7e14fa316bbd252",
+    "url": "/InteractiveResume.git/static/media/react.2f88349b.jpg"
+  },
+  {
+    "revision": "b79bd9ae47373dd419efc457ec9c7c2e",
+    "url": "/InteractiveResume.git/static/media/python.b79bd9ae.jpg"
+  },
+  {
+    "revision": "b8f99ba7a8591e7abacb696967362989",
+    "url": "/InteractiveResume.git/static/media/roxana_developer.b8f99ba7.pdf"
+  },
+  {
+    "revision": "f8522f5a9773fea09fc390253b8946a3",
+    "url": "/InteractiveResume.git/static/media/roxana_recommendation_letter_blur.f8522f5a.pdf"
+  },
+  {
+    "revision": "a83900eddfc23dbbfaf8",
+    "url": "/InteractiveResume.git/static/css/1.51025f36.chunk.css"
+  },
+  {
+    "revision": "ed1cd6691acbd43178e0cca8622615cb",
+    "url": "/InteractiveResume.git/index.html"
+  }
+];
